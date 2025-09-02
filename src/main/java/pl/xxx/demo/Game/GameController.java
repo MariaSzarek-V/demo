30075c6e5b7601a68107ad6a7767a1ba8af43b46
MariaSzarek-V <mszarek@visa.com>
2025-09-03 00:02:40 +0200

    game getAll done

--- src/main/java/pl/xxx/demo/Game/GameController.java
+++ src/main/java/pl/xxx/demo/Game/GameController.java
@@ -1,12 +1,26 @@
-//package pl.xxx.demo.Game;
-//
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//@RequestMapping("/games")
-//public class GameController {
-//
-//    private final GameService gameService;
-//
-//}
+package pl.xxx.demo.Game;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/games")
+public class GameController {
+
+    private final GameService gameService;
+
+    public GameController(GameService gameService) {
+        this.gameService = gameService;
+    }
+
+    @GetMapping("")
+    public List<Game> getAllGames() {
+        return gameService.getAll();
+    }
+
+
+
+}
--- src/main/java/pl/xxx/demo/Game/GameService.java
+++ src/main/java/pl/xxx/demo/Game/GameService.java
@@ -1,23 +1,23 @@
-//package pl.xxx.demo.Game;
-//
-//import org.springframework.stereotype.Service;
-//
-//import java.util.List;
-//import java.util.Optional;
-//
-//@Service
-//public class GameService {
-//    private final GameRepository gameRepository;
-//
-//    public GameService(GameRepository gameRepository) {
-//        this.gameRepository = gameRepository;
-//    }
-//
-//    public List<Game> getAll(){
-//        return gameRepository.findAll();
-//    }
-//
-//    public Optional<Game> get(long id){
-//        return gameRepository.findById(id);
-//    }
-//}
+package pl.xxx.demo.Game;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class GameService {
+    private final GameRepository gameRepository;
+
+    public GameService(GameRepository gameRepository) {
+        this.gameRepository = gameRepository;
+    }
+
+    public List<Game> getAll(){
+        return gameRepository.findAll();
+    }
+
+    public Optional<Game> get(long id){
+        return gameRepository.findById(id);
+    }
+}
