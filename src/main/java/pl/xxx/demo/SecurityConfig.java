3a800a0abf73533f7e5dec60ca0589fa68b61ecb
MariaSzarek-V <mszarek@visa.com>
2025-09-07 13:53:05 +0200

    securtityConfig updated err

--- src/main/java/pl/xxx/demo/SecurityConfig.java
+++ src/main/java/pl/xxx/demo/SecurityConfig.java
@@ -3,15 +3,30 @@ package pl.xxx.demo;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .securityMatcher("/api/**")
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth ->auth
+                        .anyRequest().permitAll()
+                )
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                );
+
+        http
+                .securityMatcher("/**")
+                .csrf(csrf -> csrf.ignoringRequestMatchers("/api/**"))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/", "/home", "/index").permitAll()
                         .requestMatchers("/css/**", "/js/**", "/vendor/**", "/img/**", "/scss").permitAll()
