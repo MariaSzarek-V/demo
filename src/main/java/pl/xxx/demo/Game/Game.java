8bfbdebb7f97369d798570906ed41f5b602ac1b3
MariaSzarek-V <mszarek@visa.com>
2025-09-09 22:36:40 +0200

    admin panel controll updated

--- src/main/java/pl/xxx/demo/Game/Game.java
+++ src/main/java/pl/xxx/demo/Game/Game.java
@@ -1,7 +1,10 @@
 package pl.xxx.demo.Game;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import pl.xxx.demo.Enum.GameStatus;
 
@@ -10,17 +13,20 @@ import java.time.LocalDateTime;
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Game {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank
     private String homeTeam;
+    @NotBlank
     private String awayTeam;
     private Integer homeScore;
     private Integer awayScore;
     private LocalDateTime gameDate;
     @Enumerated(EnumType.STRING)
     private GameStatus gameStatus = GameStatus.SCHEDULED;
-
 
 }
