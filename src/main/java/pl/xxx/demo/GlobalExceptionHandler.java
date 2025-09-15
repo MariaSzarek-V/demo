2cc63087c1cd4a3c394a4d97a344e6b24e58bf88
MariaSzarek-V <mszarek@visa.com>
2025-09-15 21:35:06 +0200

    dodana obsluga @Valid

--- src/main/java/pl/xxx/demo/GlobalExceptionHandler.java
+++ src/main/java/pl/xxx/demo/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package pl.xxx.demo;
 import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import pl.xxx.demo.Error.*;
@@ -47,10 +48,31 @@ public class GlobalExceptionHandler {
 
 
     private ResponseEntity<Map<String, Object>> buildErrorResponse(String message, HttpStatus status) {
+        return buildErrorResponse(message, status, null);
+    }
+
+    private ResponseEntity<Map<String, Object>> buildErrorResponse(String message, HttpStatus status, Map<String, String> errors) {
         Map<String, Object> response = new HashMap<>();
         response.put("message", message);
         response.put("status", status.value());
+        if (errors !=null && !errors.isEmpty()) {
+            response.put("errors", errors);
+        }
         return ResponseEntity.status(status).body(response);
+    }
+
+
+    /**
+     * metoda do @Valid
+     *
+     */
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, Object>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> fieldErrors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                fieldErrors.put(error.getField(), error.getDefaultMessage())
+        );
+        return buildErrorResponse("Błąd walidacji", HttpStatus.BAD_REQUEST, fieldErrors);
     }
 
 }
