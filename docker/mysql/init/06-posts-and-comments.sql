9a2b681ceec8f66a47008c1ca4fa7cf0a2bc7fc9
Maria Szarek <mszarek@visa.com>
2026-04-06 22:41:13 +0200

    dashbord update

--- docker/mysql/init/06-posts-and-comments.sql
+++ docker/mysql/init/06-posts-and-comments.sql
@@ -10,142 +10,128 @@ TRUNCATE TABLE post_comment;
 TRUNCATE TABLE post;
 SET FOREIGN_KEY_CHECKS = 1;
 
--- Posty (po 2 dla każdego użytkownika)
+-- Posty (6 postów o meczach międzynarodowych i zawodnikach)
 INSERT INTO post (title, content, image_url, created_at, updated_at, user_id) VALUES
--- admin (id=1)
-('Najlepszy gol sezonu!', 'Ten gol Lewandowskiego z przewrotki był absolutnie genialny! Nie mogę przestać tego oglądać. Co za technika! Bramkarz nie miał szans. To pokazuje różnicę między dobrym a wybitnym piłkarzem.', NULL, '2026-04-01 10:00:00', '2026-04-01 10:00:00', 1),
-('Analiza taktyczna meczu Polska-Hiszpania', 'Dzisiejszy mecz pokazał jak ważne jest posiadanie piłki. 65% posiadania przełożyło się na zwycięstwo 3-0. Nasza defensywa była solidna, a atak skuteczny. Zieliński kontrolował środek pola jak maestro.', NULL, '2026-04-02 14:30:00', '2026-04-02 14:30:00', 1),
+-- Post 1: POLSKA NIE JEDZIE NA MUNDIAL
+('POLSKA NIE JEDZIE NA MUNDIAL 2026! 😢🇵🇱', 'To się nie mogło stać... Po remisie z Holandią 1:1 i przegranej z Finlandią 2:1 nasza reprezentacja nie zakwalifikowała się do Mistrzostw Świata 2026. Mimo zwycięstw z Litwą i Maltą, zabrakło nam punktów. Lewandowski walczył do końca, Zieliński tworzył szanse, ale to nie wystarczyło. Brak nas w tym turnieju to ogromna strata dla polskiego futbolu. Teraz musimy się odbudować i skupić na kolejnych eliminacjach. Trzymajmy kciuki za naszą reprezentację! 💔', NULL, '2025-10-15 22:30:00', '2025-10-15 22:30:00', 3),
 
--- ola (id=2)
-('Mój pierwszy mecz na stadionie! 🏟️', 'Nie mogę uwierzyć! Wreszcie byłam na prawdziwym meczu! Atmosfera niesamowita, kibice śpiewali cały czas! Emocje na żywo to zupełnie co innego niż w telewizji. Polecam wszystkim takie doświadczenie!', NULL, '2026-04-01 11:00:00', '2026-04-01 11:00:00', 2),
-('Moja kolekcja koszulek piłkarskich 👕', 'Pokazuję moją kolekcję - 15 koszulek z różnych klubów i reprezentacji. Moja ulubiona to Bayern Monachium z podpisem Lewandowskiego! Którą klub macie wy?', NULL, '2026-04-02 15:00:00', '2026-04-02 15:00:00', 2),
+-- Post 2: Mecz Brazylia - Hiszpania
+('Brazylia vs Hiszpania - hit Mundialu 2026! ⚽🔥', 'To będzie mecz marzeń! Brazylia z jej legendarnymi napastnikami kontra hiszpańska tiki-taka. W grupie C Brazylia rozbiła Maroko 3:1, a Hiszpania w grupie H pokazała klasę wygrywając z Urugwajem 2:0. Jeśli obie drużyny awansują, możemy zobaczyć je w 1/8 finału. Neymar kontra Pedri, Vinicius kontra Gavi - to będą pojedynki na najwyższym poziomie!', NULL, '2026-04-01 10:00:00', '2026-04-01 10:00:00', 1),
 
--- ania (id=3)
-('Polska awansowała do półfinału! 🇵🇱', 'Historyczny moment! Polska awansowała do półfinału! Nie spałam całą noc ze szczęścia! Byliśmy niesamowici! Każdy zawodnik dał z siebie wszystko. To są wspomnienia na całe życie!', NULL, '2026-04-01 12:00:00', '2026-04-01 12:00:00', 3),
-('Mój trening przed weekendowym meczem ⚽💪', 'Dzisiaj intensywny trening - bieganie, podania, strzały. Jestem gotowa na weekend! Gram w amatorskiej lidze i mamy ważny mecz. Trzymajcie kciuki!', NULL, '2026-04-02 16:00:00', '2026-04-02 16:00:00', 3),
+-- Post 3: Niemcy wrócą po tytuł
+('Niemcy wrócą po tytuł! 🇩🇪⚽', 'Die Mannschaft jest w świetnej formie przed Mundialem 2026! W grupie E zmierzą się z Ekwadorem, Wybrzeżem Kości Słoniowej i Curacao. Müller kończy karierę po tym turnieju - to jego ostatnia szansa na kolejny mundial. Młodzi: Musiala, Wirtz, Havertz tworzą niesamowitą ofensywę. Niemcy zawsze są groźni na wielkich turniejach!', NULL, '2026-03-28 14:00:00', '2026-03-28 14:00:00', 4),
 
--- janek (id=4)
-('Ranking najlepszych drużyn w historii futbolu 🏆', 'Barcelona 2009-2011 to najlepsza drużyna w historii futbolu. Messi, Xavi, Iniesta - magia! Tiki-taka w najczystszej formie. Żadna drużyna nie grała tak pięknie. Change my mind!', NULL, '2026-04-01 13:00:00', '2026-04-01 13:00:00', 4),
-('Statystyki Lewandowskiego w tym sezonie 📈', 'Lewandowski 30 goli w sezonie! Najlepszy napastnik w Europie bez dwóch zdań! Konsekwencja, profesjonalizm i talent. Duma Polski!', NULL, '2026-04-02 17:00:00', '2026-04-02 17:00:00', 4),
+-- Post 4: Francja faworyt
+('Francja znowu faworytem! 🇫🇷👑', 'Les Bleus jadą po kolejny tytuł! Mbappé w szczytowej formie, Griezmann nadal magia, młodzi jak Camavinga i Tchouameni kontrolują środek pola. W grupie I z Senegalem i Norwegią nie powinni mieć problemów. Po triumfie w 2018 i finale w 2022, teraz czas na potwierdzenie dominacji! Deschamps wie jak wygrywać wielkie turnieje.', NULL, '2026-04-02 16:30:00', '2026-04-02 16:30:00', 7),
 
--- kuba (id=5)
-('Derby miasta - najwięcej emocji w piłce! 🔴🔵', 'Najbardziej emocjonujący mecz roku! Derby miasta zawsze dostarczają niesamowitych emocji! Rivalstwo, atmosfera, pasja - to jest istota futbolu!', NULL, '2026-04-01 14:00:00', '2026-04-01 14:00:00', 5),
-('VAR - za czy przeciw? Wasza opinia? 🤔', 'VAR poprawia sprawiedliwość, ale zabija spontaniczne emocje. Co myślicie? Czy warto poświęcić emocje dla sprawiedliwości? Ciekaw jestem waszych opinii!', NULL, '2026-04-02 18:00:00', '2026-04-02 18:00:00', 5),
+-- Post 5: Lewandowski legenda
+('Robert Lewandowski - legenda pomimo braku Mundialu! 👑⚽', 'Mimo że Polska nie awansowała na Mundial 2026, Lewandowski pozostaje absolutną legendą! 35 goli w eliminacjach, 80+ goli w reprezentacji, kapitan który zawsze dawał z siebie wszystko. W meczu z Finlandią strzelił pięknego gola, ale defensywa zawiodła. Lewy, dziękujemy za wszystko! Twoja klasa światowa to duma każdego Polaka. 🇵🇱', NULL, '2025-10-20 19:00:00', '2025-10-20 19:00:00', 9),
 
--- michal (id=6)
-('Real Madryt vs Bayern - klasyk Ligi Mistrzów! ⭐', 'Real Madryt vs Bayern Monachium - mecz na miarę finału! Taka piłka to poezja! Dwa giganty europejskiego futbolu w starciu. Nie mogę się doczekać!', NULL, '2026-04-01 15:00:00', '2026-04-01 15:00:00', 6),
-('Młode talenty które warto obserwować 🌟', 'Obserwujcie tego młodego zawodnika z Borussii! Za 2 lata będzie w TOP 10 najlepszych! Talent, szybkość i inteligencja piłkarska. Zapamiętajcie tę nazwę!', NULL, '2026-04-02 19:00:00', '2026-04-02 19:00:00', 6),
-
--- magda (id=7)
-('Kobiecy futbol w Polsce rośnie! 💪⚽', 'Coraz więcej dziewczyn gra w piłkę! To wspaniałe widzieć taki rozwój! Wreszcie kobiecy futbol dostaje uwagę jaką zasługuje. Gratuluję wszystkim piłkarkom!', NULL, '2026-04-01 16:00:00', '2026-04-01 16:00:00', 7),
-('Mundial 2026 - kto zwycięży? Prognozy 🏆', 'Kto waszym zdaniem wygra Mundial 2026? Ja stawiam na Brazylię lub Francję! Obie drużyny mają niesamowity skład. A może Polska zaskoczy?', NULL, '2026-04-02 20:00:00', '2026-04-02 20:00:00', 7),
-
--- marcin (id=8)
-('Premier League najlepsza liga świata! 🏴󠁧󠁢󠁥󠁮󠁧󠁿', 'Tempo, intensywność, wyrównanie - Premier League to najlepsza liga świata! Każdy mecz to emocje. Żadna inna liga nie ma takiej jakości od góry do dołu tabeli!', NULL, '2026-04-01 17:00:00', '2026-04-01 17:00:00', 8),
-('Manchester United wraca na szczyt! 👹', 'Ten sezon pokazuje że United wraca na szczyt! Old Trafford znów się liczy! Nowe talenty, doświadczeni gracze - idealne połączenie!', NULL, '2026-04-02 21:00:00', '2026-04-02 21:00:00', 8),
-
--- lukasz (id=9)
-('Bundesliga niedoceniana 🇩🇪', 'Bayern dominuje, ale inne kluby też grają świetny futbol! Bundesliga zasługuje na więcej uwagi! Taktyka, technika i atmosfera na stadionach - rewelacja!', NULL, '2026-04-01 18:00:00', '2026-04-01 18:00:00', 9),
-('Robert Lewandowski - polska legenda! 👑', 'Najlepszy polski piłkarz wszech czasów! Dumny z naszego kapitana! Rekordy, gole, trofea. Reprezentuje nas na najwyższym poziomie!', NULL, '2026-04-02 22:00:00', '2026-04-02 22:00:00', 9),
-
--- pawel (id=10)
-('Serie A - taktyczna szkoła futbolu 🇮🇹', 'Włoski futbol to szkoła taktyki. Każdy mecz to szachowa partia! Catenaccio, pressing, kontratak - wszystko dopracowane do perfekcji!', NULL, '2026-04-01 19:00:00', '2026-04-01 19:00:00', 10),
-('AC Milan powraca do europejskiej elity 🔴⚫', 'Milan wraca do europejskiej elity! Rossoneri znów budzą respekt! Młody skład, świetny trener - przyszłość wygląda obiecująco!', NULL, '2026-04-02 23:00:00', '2026-04-02 23:00:00', 10);
+-- Post 6: Holandia - Japonia zapowiedź
+('Holandia - Japonia: Klasyk mundialu! 🇳🇱⚽🇯🇵', 'Mecz otwarcia grupy F zapowiada się rewelacyjnie! Holandia po remisie z Polską 1:1 w eliminacjach pokazała solidną grę. Van Dijk wrócił do formy, Gakpo strzela gole. Japonia zawsze jest niewygodnym rywalem - szybka, techniczna, zorganizowana. Pamiętacie ich zwycięstwo nad Niemcami w 2022? To będzie emocjonujące starcie 14 czerwca 2026!', NULL, '2026-04-05 11:00:00', '2026-04-05 11:00:00', 6);
 
 -- Reakcje do postów (około 30%)
 INSERT INTO post_reactions (post_id, emoji, username) VALUES
--- Post 1 (admin - gol sezonu) - 5 osób
-(1, '⚽', 'ania'),
-(1, '🔥', 'janek'),
-(1, '😍', 'kuba'),
-(1, '👏', 'magda'),
-(1, '💯', 'lukasz'),
+-- Post 1 (Polska nie jedzie na mundial) - 8 osób
+(1, '😢', 'admin'),
+(1, '💔', 'ola'),
+(1, '😭', 'janek'),
+(1, '🇵🇱', 'kuba'),
+(1, '😔', 'michal'),
+(1, '💪', 'magda'),
+(1, '😥', 'marcin'),
+(1, '😢', 'lukasz'),
 
--- Post 3 (ola - stadion) - 4 osoby
-(3, '🏟️', 'admin'),
-(3, '🎉', 'ania'),
-(3, '❤️', 'janek'),
-(3, '👍', 'magda'),
+-- Post 2 (Brazylia vs Hiszpania) - 6 osób
+(2, '🔥', 'ania'),
+(2, '⚽', 'janek'),
+(2, '😍', 'kuba'),
+(2, '👏', 'magda'),
+(2, '💯', 'lukasz'),
+(2, '🏆', 'pawel'),
 
--- Post 5 (ania - awans) - 6 osób
-(5, '🇵🇱', 'admin'),
-(5, '🎉', 'ola'),
-(5, '🔥', 'janek'),
-(5, '❤️', 'kuba'),
-(5, '👏', 'michal'),
-(5, '😍', 'lukasz'),
+-- Post 3 (Niemcy) - 5 osób
+(3, '🇩🇪', 'admin'),
+(3, '⚽', 'ola'),
+(3, '🔥', 'ania'),
+(3, '👍', 'michal'),
+(3, '💪', 'lukasz'),
 
--- Post 7 (janek - Barcelona) - 4 osoby
-(7, '🏆', 'admin'),
-(7, '😍', 'ola'),
-(7, '⚽', 'lukasz'),
-(7, '💯', 'pawel'),
+-- Post 4 (Francja) - 7 osób
+(4, '🇫🇷', 'admin'),
+(4, '🏆', 'ola'),
+(4, '⚽', 'janek'),
+(4, '🔥', 'kuba'),
+(4, '👑', 'michal'),
+(4, '😍', 'marcin'),
+(4, '💯', 'pawel'),
 
--- Post 10 (kuba - VAR) - 5 osób
-(10, '🤔', 'admin'),
-(10, '👍', 'ania'),
-(10, '👎', 'magda'),
-(10, '❓', 'marcin'),
-(10, '💭', 'piotr'),
+-- Post 5 (Lewandowski legenda) - 9 osób
+(5, '👑', 'admin'),
+(5, '❤️', 'ola'),
+(5, '🇵🇱', 'ania'),
+(5, '⚽', 'janek'),
+(5, '💪', 'kuba'),
+(5, '😍', 'michal'),
+(5, '👏', 'magda'),
+(5, '🔥', 'marcin'),
+(5, '💯', 'pawel'),
 
--- Post 13 (magda - kobiecy futbol) - 4 osoby
-(13, '💪', 'admin'),
-(13, '❤️', 'ola'),
-(13, '👏', 'ania'),
-(13, '🔥', 'janek'),
-
--- Post 16 (marcin - Premier League) - 5 osób
-(16, '🏴󠁧󠁢󠁥󠁮󠁧󠁿', 'admin'),
-(16, '⚽', 'janek'),
-(16, '🔥', 'kuba'),
-(16, '👍', 'lukasz'),
-(16, '💯', 'pawel');
+-- Post 6 (Holandia - Japonia) - 5 osób
+(6, '🇳🇱', 'admin'),
+(6, '🇯🇵', 'ania'),
+(6, '⚽', 'janek'),
+(6, '🔥', 'magda'),
+(6, '👍', 'lukasz');
 
 -- Komentarze do postów
 INSERT INTO post_comment (text, created_at, user_id, post_id, parent_comment_id, quoted_comment_id) VALUES
--- Komentarze do posta 1 (gol sezonu)
-('Zgadzam się w 100%! Ten gol był niesamowity! 🔥', '2026-04-01 10:30:00', 2, 1, NULL, NULL),
-('Lewandowski to prawdziwa legenda polskiego futbolu! 👑', '2026-04-01 11:00:00', 4, 1, NULL, NULL),
-('Widziałem to na żywo na stadionie! Cały stadion oszalał!', '2026-04-01 11:30:00', 5, 1, NULL, NULL),
-('Dokładnie! Ja też tam byłam! Niesamowite przeżycie! 🏟️', '2026-04-01 12:00:00', 2, 1, 3, NULL),
+-- Komentarze do posta 1 (Polska nie jedzie na mundial)
+('To jest tragedia dla polskiego futbolu 😢', '2025-10-15 22:45:00', 1, 1, NULL, NULL),
+('Nie mogę w to uwierzyć... Tak bardzo chciałem zobaczyć Lewego na mundialu 💔', '2025-10-15 23:00:00', 4, 1, NULL, NULL),
+('Defensywa zawiodła w kluczowych momentach. Musimy to naprawić!', '2025-10-15 23:15:00', 8, 1, NULL, NULL),
+('Ja też płakałam... Ale wierzę że wrócimy silniejsi! 🇵🇱💪', '2025-10-15 23:30:00', 2, 1, 2, NULL),
 
--- Komentarze do posta 5 (awans Polski)
-('Nie spałam całą noc! Jakie emocje! 🇵🇱', '2026-04-01 12:30:00', 1, 5, NULL, NULL),
-('Zasłużony awans! Grali wspaniale!', '2026-04-01 13:00:00', 4, 5, NULL, NULL),
-('Zieliński był MVP tego meczu! ⚡', '2026-04-01 13:30:00', 9, 5, NULL, NULL),
+-- Komentarze do posta 2 (Brazylia vs Hiszpania)
+('To będzie mecz stulecia! Neymar vs Pedri 🔥', '2026-04-01 10:30:00', 3, 2, NULL, NULL),
+('Stawiam na Brazylię! Mają zbyt silny atak', '2026-04-01 11:00:00', 5, 2, NULL, NULL),
+('Hiszpania pokaże tiki-taka i wygra 2:1', '2026-04-01 11:30:00', 10, 2, NULL, NULL),
 
--- Komentarze do posta 7 (Barcelona)
-('Messi w tamtej Barcelonie to była magia! ✨', '2026-04-01 13:30:00', 2, 7, NULL, NULL),
-('Xavi i Iniesta w środku pola - mistrzostwo świata!', '2026-04-01 14:00:00', 6, 7, NULL, NULL),
-('Ta Barcelona zmieniła futbol na zawsze', '2026-04-01 14:30:00', 10, 7, NULL, NULL),
+-- Komentarze do posta 4 (Francja)
+('Mbappé w takiej formie jest nie do zatrzymania! 🚀', '2026-04-02 16:45:00', 1, 4, NULL, NULL),
+('Francja ma najlepszy skład na mundialu. Będą w finale!', '2026-04-02 17:00:00', 6, 4, NULL, NULL),
+('Deschamps to trener mistrzów! 🏆', '2026-04-02 17:15:00', 9, 4, NULL, NULL),
 
--- Komentarze do posta 10 (VAR)
-('Jestem za VAR! Mniej błędów sędziowskich!', '2026-04-02 18:30:00', 1, 10, NULL, NULL),
-('Ale zabija emocje... Trudna decyzja', '2026-04-02 19:00:00', 3, 10, NULL, NULL),
-('Sprawiedliwość ważniejsza niż emocje', '2026-04-02 19:30:00', 6, 10, NULL, NULL),
-('Nie zgadzam się. Emocje to istota futbolu!', '2026-04-02 20:00:00', 7, 10, 12, NULL);
+-- Komentarze do posta 5 (Lewandowski legenda)
+('Lewandowski to najlepszy polski piłkarz w historii! 👑', '2025-10-20 19:30:00', 1, 5, NULL, NULL),
+('Bez niego byłoby jeszcze gorzej. Dziękujemy Kapitanie! ❤️', '2025-10-20 19:45:00', 3, 5, NULL, NULL),
+('80 goli w reprezentacji to niesamowite osiągnięcie!', '2025-10-20 20:00:00', 4, 5, NULL, NULL),
+('Szacunek za walkę do końca! 🇵🇱⚽', '2025-10-20 20:15:00', 7, 5, NULL, NULL);
 
 -- Reakcje do komentarzy
 INSERT INTO post_comment_reactions (post_comment_id, emoji, username) VALUES
--- Komentarz 1 (o golu)
-(1, '👍', 'admin'),
-(1, '🔥', 'janek'),
-(1, '⚽', 'kuba'),
+-- Komentarz 1 (tragedia dla futbolu)
+(1, '😢', 'ola'),
+(1, '💔', 'ania'),
+(1, '😭', 'janek'),
 
--- Komentarz 5 (nie spałam)
-(5, '😂', 'ola'),
-(5, '❤️', 'ania'),
-(5, '🎉', 'janek'),
+-- Komentarz 2 (Lewy na mundialu)
+(2, '😢', 'admin'),
+(2, '❤️', 'ania'),
+(2, '🇵🇱', 'michal'),
 
--- Komentarz 8 (Messi magia)
-(8, '✨', 'admin'),
-(8, '😍', 'ania'),
-(8, '⚽', 'lukasz'),
+-- Komentarz 5 (mecz stulecia)
+(5, '🔥', 'admin'),
+(5, '⚽', 'janek'),
+(5, '😍', 'lukasz'),
 
--- Komentarz 11 (VAR za)
-(11, '👍', 'janek'),
-(11, '💯', 'michal'),
+-- Komentarz 8 (Mbappé)
+(8, '🚀', 'ania'),
+(8, '⚽', 'kuba'),
+(8, '💯', 'lukasz'),
 
--- Komentarz 12 (VAR zabija emocje)
-(12, '❤️', 'magda'),
-(12, '👏', 'marcin');
+-- Komentarz 11 (Lewandowski najlepszy)
+(11, '👑', 'ola'),
+(11, '❤️', 'ania'),
+(11, '🇵🇱', 'janek'),
+(11, '💪', 'magda');
