002b93f798e928b28f4b38741c170b18f11b71d7
Maria Szarek <mszarek@visa.com>
2026-04-20 23:15:50 +0200

    check

--- docker/mysql/init-old/07-chat-messages.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Wrapper script to execute 07-chat-messages.sql with proper UTF-8 encoding
-
-echo "Executing 07-chat-messages.sql with UTF-8 encoding..."
-
-# Execute the SQL file with explicit UTF-8 encoding
-mysql --protocol=socket -uroot -p"${MYSQL_ROOT_PASSWORD}" --default-character-set=utf8mb4 "${MYSQL_DATABASE}" < /docker-entrypoint-initdb.d/07-chat-messages.sql.data
-
-echo "07-chat-messages.sql executed successfully"
